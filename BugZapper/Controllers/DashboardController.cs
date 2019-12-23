7e974ba15e22c976d1e6148ed65725cf784c0ea3
rickpina <rickapina@yahoo.com>
2019-12-23 04:56:05 -0500

    First commite

---
+++ BugZapper/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BugZapper.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // 
+        // GET: /Dashboard/
+
+       // public IActionResult Index()
+       // {
+       //     return View();
+       // }
+
+        // 
+        // GET: /Dashboard/Welcome/ 
+
+        // Continue Here me in the future :)
+        //https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-controller?view=aspnetcore-3.1&tabs=visual-studio
+
+
+        public string Welcome(string name, int ID = 1)
+        {
+           // return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
